b275a5731cf500041dc64d27791c5a6f33813cbc
Greg Cormier <gcormier@gmail.com>
2019-07-09 11:32:45 -0700

    compact

--- azure/packer-ubuntu.sh
+++ azure/packer-ubuntu.sh
@@ -5,12 +5,7 @@
 # Get stuff
 sudo apt-get -yqq update
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -yqq install cmake git makedepf90 gfortran gcc patch htop iptraf-ng zlib1g-dev libcurl4-openssl-dev pkg-config git gcc gcc-opt cmake patch autoconf flex librdmacm-dev libnuma-dev doxygen
-#TODO: Instal texlive and supress the prompt that comes up for service restart
-#sudo apt-get -yqq install  texlive-latex-base
-sudo apt-get -yqq install nvidia-cuda-dev
-
-sudo apt-get -yqq install libfabric-dev # Not needed if EFA is installed, fails gracefully
+sudo apt-get -yqq install cmake git makedepf90 gfortran gcc patch htop iptraf-ng zlib1g-dev libcurl4-openssl-dev pkg-config git gcc gcc-opt cmake patch autoconf flex librdmacm-dev libnuma-dev doxygen nvidia-cuda-dev texlive-latex-base libfabric-dev
 
 # AzCopy Install
 wget --quiet --content-disposition https://aka.ms/downloadazcopy-v10-linux && tar zxvf azcopy_linux_amd64_*.tar.gz && sudo cp azcopy_linux_amd64*/azcopy /usr/local/bin/
@@ -23,10 +18,7 @@ cd
 
 # Make OpenMPI - (c5.18xl 3m30s)
 wget --quiet https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.1.tar.gz && tar zxvf openmpi-4.0.1.tar.gz && cd openmpi-4.0.1
-# AWS EFA
-./configure --prefix=/usr --enable-static --enable-shared --with-cuda=/usr/include --with-libfabric=/opt/amazon/efa
-# Other
-#./configure --prefix=/usr --enable-static --enable-shared --with-cuda=/usr/include
+./configure --prefix=/usr --enable-static --enable-shared --with-cuda=/usr/include
 
 make all -j && sudo make install
 cd
@@ -36,7 +28,6 @@ wget --quiet https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.
 ./configure --prefix=/usr --enable-fortran --enable-shared --enable-static #--enable-parallel
 make -j && sudo make install
 cd
-
 
 # NetCDF
 wget --quiet https://github.com/Unidata/netcdf-c/archive/v4.7.0.tar.gz && tar zxvf v4.7.0.tar.gz && cd netcdf-c-4.7.0
