eb39ffd536e25bd7746c808621179f44b62d8659
Ubuntu <greg@tennis.vfdwdm4fqt3utklkgl4rr5lrie.ux.internal.cloudapp.net>
2019-08-28 17:18:31 +0000

    woops... keep the limits in packer. pkey only in ansible.

---
+++ azure/packer-infiniband.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Updating limits.conf for InifiniBand..."
+cat << EOF | sudo tee -a /etc/security/limits.conf
+*               hard    memlock         unlimited
+*               soft    memlock         unlimited
+*               hard    nofile          65535
+*               soft    nofile          65535
+EOF
