4751d8e4cdceb83e06d384ab2133f829871bae77
Greg@tennis <gcormier@gmail.com>
2019-11-22 19:01:50 +0000

    finally get latest versions of most and confirmed working

--- azure/packer-ubuntu.sh
+++ azure/packer-ubuntu.sh
@@ -19,24 +19,24 @@ cd
 
 # OpenMPI
 wget --quiet https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.2.tar.gz && tar zxvf openmpi-4.0.2.tar.gz && cd openmpi-4.0.2
-./configure --prefix=/usr --enable-static --enable-shared --with-cuda=/usr/include
-make all -j && sudo make install
+./configure --prefix=/usr --enable-static --enable-shared --with-cuda=/usr/include # works
+make all -j && sudo make install #works
 cd
 
 # HDF5
 wget --quiet https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.5/src/hdf5-1.10.5.tar.gz && tar zxvf hdf5-1.10.5.tar.gz && cd hdf5-1.10.5/
 ./configure --prefix=/usr --enable-fortran --enable-shared --enable-static #--enable-parallel
-make -j && sudo make install
+make -j && sudo make install #WORKS
 cd
 
 # NetCDF
-wget --quiet https://github.com/Unidata/netcdf-c/archive/v4.7.2.tar.gz && tar zxvf v4.7.2.tar.gz && cd netcdf-c-4.7.2
+wget --quiet https://github.com/Unidata/netcdf-c/archive/v4.7.3.tar.gz && tar zxvf v4.7.3.tar.gz && cd netcdf-c-4.7.3
 ./configure --prefix=/usr
-make -j `nproc` && sudo make install
+make -j `nproc` && sudo make install # WORKS
 cd
 
 # NetCDF-Fortran
-wget --quiet https://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-fortran-4.7.2.tar.gz && tar zxvf netcdf-fortran-4.7.2.tar.gz && cd netcdf-fortran-4.7.2/
+wget --quiet https://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-fortran-4.5.2.tar.gz && tar zxvf netcdf-fortran-4.5.2.tar.gz && cd netcdf-fortran-4.5.2/
 ./configure --prefix=/usr 
 make -j `nproc` && sudo make install
 cd
@@ -45,3 +45,4 @@ cd
 git clone https://github.com/intel/opa-mpi-apps/ && cd opa-mpi-apps/MpiApps/apps/imb/src
 make CC=mpicc
 sudo cp IMB-MPI1 /usr/local/bin
+cd
